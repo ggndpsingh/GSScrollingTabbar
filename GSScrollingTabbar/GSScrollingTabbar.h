723244948e80fd127ebe207f3a0d42f0ec173123
Gagan Deep <gagan.deep@me.com>
2018-07-05 19:43:30 +1000

    Initial Commit

---
+++ GSScrollingTabbar/GSScrollingTabbar.h
@@ -0,0 +1,19 @@
+//
+//  GSScrollingTabbar.h
+//  GSScrollingTabbar
+//
+//  Created by Gagandeep Singh on 5/7/18.
+//  Copyright © 2018 Gagandeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSScrollingTabbar.
+FOUNDATION_EXPORT double GSScrollingTabbarVersionNumber;
+
+//! Project version string for GSScrollingTabbar.
+FOUNDATION_EXPORT const unsigned char GSScrollingTabbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSScrollingTabbar/PublicHeader.h>
+
+
